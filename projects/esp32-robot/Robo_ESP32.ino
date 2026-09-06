187965a808b338aa67a476a5165af7063d22bda4
VIVEK <vivekhr36.2007@gmail.com>
2026-09-06 16:42:59 +0530

    commit

--- projects/esp32-robot/Robo_ESP32.ino
+++ projects/esp32-robot/Robo_ESP32.ino
@@ -6,11 +6,8 @@
  * =========================================================================
  */
 
-#include "BluetoothSerial.h"
-
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to enable it.
-#endif
+#include <Arduino.h>
+#include <BluetoothSerial.h>
 
 BluetoothSerial SerialBT;
 
